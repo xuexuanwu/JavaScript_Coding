7f4befa245ed7511f6ea6c0bc2386f717fcef403
test <cao896@usc.edu>
2019-03-27 14:23:34 -0700

    one

--- reStrKeepSign.js
+++ reStrKeepSign.js
@@ -8,7 +8,7 @@ const reStrKeepSign = (s) => {
               [a[i], a[j]] = [a[j], a[i]];
               i++;
               j--;
-          } else if (a[j].search(/[a-zA-Z]/) === -1) {
+          } else if (a[j].search(/[1231231]/) === -1) {
               j--;
           }
       } else if (a[i].search(/[a-zA-Z]/) === -1) {
@@ -16,6 +16,7 @@ const reStrKeepSign = (s) => {
       }
     }
     console.log(a);
+    console.log(b)
     return a.join('');
 
 }
