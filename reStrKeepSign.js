6751fa5e7e4ecc17b4c2d6fab3ff849f8631ee20
XUE XUAN WU <XUEXUANWU@xuedembp.attlocal.net>
2019-03-27 14:13:29 -0700

    new one

---
+++ reStrKeepSign.js
@@ -0,0 +1,22 @@
+const reStrKeepSign = (s) => {
+    let i = 0;
+    let j = s.length - 1;
+    let a = s.split('');
+    while(i <= j) {
+      if (a[i].search(/[a-zA-Z]/) === 0) {   //alpha
+          if (a[j].search(/[a-zA-Z]/) === 0) {  //alpha
+              [a[i], a[j]] = [a[j], a[i]];
+              i++;
+              j--;
+          } else if (a[j].search(/[a-zA-Z]/) === -1) {
+              j--;
+          }
+      } else if (a[i].search(/[a-zA-Z]/) === -1) {
+          i++;
+      }
+    }
+    console.log(a);
+    return a.join('');
+
+}
+console.log(reStrKeepSign('Abc:def@gh$ijk')); //  output 'kji:hgf@ed$cbA'
