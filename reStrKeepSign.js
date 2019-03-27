836eac9788b43d262236d31e6808774fb28a01fb
Winnie Wu <xuw28@pitt.edu>
2019-03-27 16:47:30 -0700

    Update reStrKeepSign.js

--- reStrKeepSign.js
+++ reStrKeepSign.js
@@ -8,7 +8,7 @@ const reStrKeepSign = (s) => {
               [a[i], a[j]] = [a[j], a[i]];
               i++;
               j--;
-          } else if (a[j].search(/[1231231]/) === -1) {
+          } else if (a[j].search(/[a-zA-Z]/) === -1) {
               j--;
           }
       } else if (a[i].search(/[a-zA-Z]/) === -1) {
